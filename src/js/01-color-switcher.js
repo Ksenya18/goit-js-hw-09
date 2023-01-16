cf1954c64d33f63e85a151ab213fe514ee79692a
Ksenya18 <106611808+Ksenya18@users.noreply.github.com>
2023-01-17 00:30:10 +0200

    goit-js-hw-09 updated

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
+startBtn.disabled = false;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+    let color = getRandomHexColor ();
+document.body.style.backgroundColor = color;
+    }, 1000);
+startBtn.disabled = true;
+stopBtn.disabled = false;
+});
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+startBtn.disabled = false;
+stopBtn.disabled = true;
+  });
+
+  
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector ('.form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+formEl.addEventListener('submit',  e => {
+  e.preventDefault();
+
+  let delay = Number(delayEl.value);
+  let step = Number(stepEl.value);
+  let amount = Number(amountEl.value);
+
+  for (let i = 1; i <= amount; i += 1){
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    delay += step;
+    
+  };
+});
+
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay });
   } else {
-    // Reject
+    reject({ position, delay });
   }
+}, delay);
+});
 }
+
+
+
